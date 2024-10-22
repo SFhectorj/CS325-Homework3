5eaea8ab6908671e07188fbd60c51aa23cc9044b
github_username <github_email>
2024-10-21 18:26:23 -0700

    Question 1

--- DNAMatch.py
+++ DNAMatch.py
@@ -2,6 +2,10 @@
 # TOP-DOWN Approach
 
 def dna_match_topdown(DNA1, DNA2):
+    """
+    This function uses a top-down approach of dynamic programming to
+    find similarities between two sequences.
+    """
 
     memo = {}
 
